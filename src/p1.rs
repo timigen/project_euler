c170ee55eab25c269a7dfd9deaf896c6476556af
Timigen <timigen@gmail.com>
2017-12-17 21:57:32 -0500

    p1 naive

--- src/p1.rs
+++ src/p1.rs
@@ -1,12 +1,11 @@
+pub fn naive() {
+    let mut sum:i32 = 0;
 
-    pub fn naive() {
-        let mut sum:i32 = 0;
-
-        for x in 0..1000 {
-            if x % 3 == 0 || x % 5 == 0 {
-                sum += x;
-            }
+    for x in 0..1000 {
+        if x % 3 == 0 || x % 5 == 0 {
+            sum += x;
         }
-
-        println!("sum {}", sum);
     }
+    
+    println!("sum {}", sum);
+}
